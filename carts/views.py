a397c8f2de39fbbb2a845334045dbcf60f39132b
arvindrk47 <arvindrk47@gmail.com>
2023-12-16 00:44:13 +0530

    fixed some bugs

--- carts/views.py
+++ carts/views.py
@@ -38,5 +38,20 @@ def add_cart(request, product_id):
 
 
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items =None):
+    try:
+        cart_items = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except Cart.DoesNotExist:
+        total = quantity = cart_items = None
+
+    context = {
+        'total': total,
+        'quantity':  quantity,
+        'cart_items': cart_items,
+    }
+    return render(request, 'store/cart.html', context)
+
