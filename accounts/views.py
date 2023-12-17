3e8381580ba8b782d0dc826ab1adb465e0df2ebd
arvindrk47 <arvindrk47@gmail.com>
2023-12-17 07:59:33 +0530

    Started account pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    return render('accounts/register.html')
+
+
+def signin(request):
+    pass
+
+def signup(request):
+    pass
